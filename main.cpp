1772177fd03091f7834550fc1ace3d0ef2ce340f
Nguyễn Tiến Dũng <88418919+dung195@users.noreply.github.com>
2022-02-25 22:45:12 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,26 +3,26 @@ using namespace std;
 #define ll long long
 #define ld long double
 #define pq priority_queue
-#define inf 1000000
-#define pii pair<int,int>
-vector<pair<int,int>> graph[inf];
-int D[inf];
-int trace[inf];
-void Dijkstra(int start,int nodes){
-    for(int i=0;i<nodes+2;i++){
-      D[i]=inf;
+#define inf 1000
+#define pii pair<ll,ll>
+vector<pair<ll,ll>> graph[inf];
+vector<ll>D;
+ll trace[inf];
+void Dijkstra(ll start,ll nodes){
+    for(ll i=0;i<nodes+2;i++){
+      D.push_back(inf);
     }
     D[start]=0;
     trace[start]=-1;
-    pq<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
+    pq<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> q;
     q.push({0,start});
     while (q.size()!=0)
     {
-      int u=q.top().second;
+      ll u=q.top().second;
       q.pop();
-      for(int i=0;i<graph[u].size();i++){
-        int v=graph[u][i].second;
-        int w=graph[u][i].first;
+      for(ll i=0;i<graph[u].size();i++){
+        ll v=graph[u][i].second;
+        ll w=graph[u][i].first;
         if(D[v]>D[u]+w){
           D[v]=D[u]+w;
           q.push({D[v],v});
@@ -32,40 +32,40 @@ void Dijkstra(int start,int nodes){
     }
     
 }
-int main(){
+main(){
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   cout.tie(0);
-  int n,m,k;
+  ll n,m,k;
   cin>>n>>m>>k;
   // freopen("main.inp","r",stdin);
   // freopen("main.out","w",stdout);
-  for(int i=0;i<m;i++){
-     int u,v,c;
+  for(ll i=0;i<m;i++){
+     ll u,v,c;
      cin>>u>>v>>c;
      pii o={c,v};
      pii p={c,u};
      graph[u].push_back(o);
      graph[v].push_back(p);
   }
-  for(int i=0;i<k;i++){
-    int k0,u,v;
+  for(ll i=0;i<k;i++){
+    ll k0,u,v;
     cin>>k0>>u>>v;
     if(k0==0){
       Dijkstra(u,n);
       cout<<D[v]<<endl;
     }
     if(k0==1){
-      int cur=v;
-      int way[inf];
-      int cnt=0;
+      ll cur=v;
+      vector<ll>way;
+      ll cnt=0;
       while(cur!=-1){
-          ++cnt;
-          way[cnt]=cur;
+          cnt++;
+          way.push_back(cur);
           cur=trace[cur];
       }
       cout<<cnt<<" ";
-      for(int i=cnt;i>=1;i--){
+      for(ll i=cnt-1;i>=0;i--){
             cout<<way[i]<<" ";
       }
       cout<<endl;
