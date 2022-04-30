54d091babd87623c587441c61cca354034b5fd3d
Nguyễn Tiến Dũng <88418919+dung195@users.noreply.github.com>
2022-04-30 15:37:34 +0700

    update

--- main.cpp
+++ main.cpp
@@ -4,42 +4,33 @@ using namespace std;
 #define ld long double
 #define pq priority_queue
 #define pii pair<int,int>
-#define inf 10000000
-#define size 100001
-int timed[100005];
-int t[100005];
-int F[100005];
-int find(int n){
-    if(F[n]!=0) return F[n];
-    if(n==1) return timed[1];
-    if(n==0) return t[1];
-    if(n<0) return inf;
-    else{
-       int a=find(n-1)+timed[n];
-       int b=find(n-2)+t[n];
-       F[n]=min(a,b);
-       return F[n];
+#define pll pair<ll,ll>
+#define inf 1e9+11
+#define sized 100005
+#define MOD 20122007
+vector<int>x,y;
+main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("main.inp","r",stdin);
+    // freopen("main.out","w",stdout);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int a,b;
+        cin>>a>>b; 
+        x.push_back(a);
+        y.push_back(b);
     }
-}
-int main(){
-   ios_base::sync_with_stdio(0);
-   cin.tie(0);
-   cout.tie(0);
-   freopen("main.inp","r",stdin);
-   freopen("main.out","w",stdout);
-   int n;
-   cin>>n;
-   for(int i=0;i<n;i++){
-      int a;
-      cin>>a;
-      timed[n-i+1]=a;
-   }
-   for(int i=1;i<n;i++){
-      int r;
-      cin>>r;
-      t[n-i]=r;
-   }
-   cout<<find(n)<<endl;
-   return 0;
-
+    sort(y.begin(),y.end());
+    int ans=inf;
+    for (int  i = 0; i < x.size(); i++)         
+    {
+        int f=x[i];
+        int pos=int(lower_bound(y.begin(),y.end(),-f)-y.begin());
+        ans=min(min(ans,abs(f+y[pos])),abs(f+y[pos-1]));
+    }
+    cout<<ans<<endl;
+    return 0;
 }
