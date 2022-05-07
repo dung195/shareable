ceb3ed7f99f87d5569780bb2d45a3aa0464517bf
Nguyễn Tiến Dũng <88418919+dung195@users.noreply.github.com>
2022-05-07 21:10:49 +0700

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -5,32 +5,45 @@ using namespace std;
 #define pq priority_queue
 #define pii pair<int,int>
 #define pll pair<ll,ll>
-#define inf 1e9+11
-#define sized 100005
-#define MOD 20122007
-vector<int>x,y;
+#define inf 2e10+11
+#define sized 10005
+int X[sized];
+int pa[sized];
+int pb[sized];
+bool check(string s){
+    int dem=0;
+    for(int i=0;i<s.size();i++){
+        if(dem<0) return false;
+        else{
+            if(s[i]==40) dem++;
+            else dem--;
+        }
+    }
+    if(dem==0) return true;
+
+    return false;
+}
+int ans=0;
+void backtrack(int k){
+    for(int i=0;i<1;i++){
+        
+    }
+}
 main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    // freopen("main.inp","r",stdin);
-    // freopen("main.out","w",stdout);
-    int n;
-    cin>>n;
+    freopen("main.inp","r",stdin);
+    freopen("main.out","w",stdout);
+    int n,m;
+    cin>>n>>m;
     for(int i=0;i<n;i++){
-        int a,b;
-        cin>>a>>b; 
-        x.push_back(a);
-        y.push_back(b);
+        cin>>pa[i];
     }
-    sort(y.begin(),y.end());
-    int ans=inf;
-    for (int  i = 0; i < x.size(); i++)         
-    {
-        int f=x[i];
-        int pos=int(lower_bound(y.begin(),y.end(),-f)-y.begin());
-        ans=min(min(ans,abs(f+y[pos])),abs(f+y[pos-1]));
+    for(int i=0;i<n;i++){
+        cin>>pb[i];
     }
+    backtrack(1);
     cout<<ans<<endl;
     return 0;
 }
