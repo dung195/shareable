4d0b27db5a373b5b4ba33d770fe5ade01a3baeb2
Nguyễn Tiến Dũng <88418919+dung195@users.noreply.github.com>
2022-06-27 11:31:31 +0700

    Dquery

--- main.cpp
+++ main.cpp
@@ -3,56 +3,93 @@ using namespace std;
 #define ll long long
 #define ld long double
 #define pq priority_queue
-#define pii pair<ll ,ll >
-#define inf 10e9+7
-#define maxn 1000000
-vector<pair<int,int> >graph[100005];
-int D[1000005];
-int way[1000005];
-void Dijkstra(int S){
-    D[S]=0;
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
-    q.push({0,S});
-    while (q.empty()!=true)
-    { 
-       int u=q.top().second;
-       q.pop();
-       for(int i=0;i<graph[u].size();i++){
-             int v=graph[u][i].second;
-             int w=graph[u][i].first;
-             if(D[v]>D[u]+w){
-                D[v]=D[u]+w;
-                q.push({D[v],v});
-                way[v]+=way[u];
-                }
-             }
-    }
+#define pii pair<ll, ll>
+#define pll pair<ll, ll>
+#define bs binary_search
+typedef vector<vector<int>> vvi;
+const ll sized = 39005;
+const ll N = 1e6 +1;
+const ll inf = 1e17;
+const ll MOD = 2111992;
+const ll LOG = 20;
+int bang[sized];
+int dem[N];
+int n,q;
+vector<pair<pii,int> >tv;
+int fre[N];
+bool cmp(pair<pii,int> a,pair<pii,int> b){
+    int divide=pow(n,1/2);
+    if(a.first.first/divide!=b.first.first/divide) return (a.first.first/divide <b.first.first/divide);
+    else return a.first.second>b.first.second;
 }
 main(){
-   ios_base::sync_with_stdio(0);
-   cin.tie(0);
-   cout.tie(0);
-   // freopen("main.inp","r",stdin);
-   // freopen("main.out","w",stdout);
-   int n,m;
-   cin>>n>>m;
-   for(int i=0;i<n+3;i++) D[i]=inf;
-   for(int i=0;i<m;i++){
-      int k;
-      cin>>k;
-      int u,v,l;
-      cin>>u>>v>>l;
-      pii o={l,v};
-      pii p={l,u};
-      if(k==1){
-         graph[u].push_back(o);
-      }
-      if(k==2){
-         graph[u].push_back(o);
-         graph[v].push_back(p);
-      }
-   }
-   way[1]=1;
-   Dijkstra(1);
-   cout<<D[n]<<" "<<way[n]<<endl; 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("main.inp","r",stdin);
+	// freopen("main.out","w",stdout);
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cin>>bang[i];
+    }
+    cin>>q;
+    int chia=pow(n,1/2);
+    for(int i=0;i<q;i++){
+        int a,b;
+        cin>>a>>b;
+        tv.push_back({{a,b},i+1});
+    }
+    sort(tv.begin(),tv.end(),cmp);
+    int l=tv[0].first.first;
+    int r=tv[0].first.second;
+    int res =0;
+    for(int i=l;i<=r;i++){
+        fre[bang[i]]++;
+        if(fre[bang[i]]==1) res++;
+    }
+    dem[tv[0].second]=res;
+    // cout<<s.size()<<endl;
+    for(int i=1;i<q;i++){
+        int start=tv[i].first.first;
+        int finish=tv[i].first.second;
+        int tvid=tv[i].second;
+        if(l>start){
+            for (int j = l-1; j >= start; j--)
+            {
+                fre[bang[j]]++;
+                if(fre[bang[i]]==1) res++;
+
+            }
+        }
+        else if(l<start){
+
+            for(int j=l;j<=start-1;j++){
+                fre[bang[j]]--;
+                if(fre[bang[j]]==0) res--;
+            }
+        }
+        l=start;
+        if(r>finish){
+
+            for (int j=r;j>=finish+1;j--)
+            {
+               fre[bang[j]]--;
+                if(fre[bang[j]]==0) res--;
+            }
+        }
+        else if(r<finish){
+
+            for(int j=r+1;j<=finish;j++){
+                 fre[bang[j]]++;
+                if(fre[bang[i]]==1) res++;
+            }
+        }
+        r=finish;
+        dem[tvid]=res;
+
+        // cout<<s.size()<<endl;
+    }
+    for(int i =1; i<=q; i++ )
+    cout<<dem[i]<<endl;
+    return 0;
 }
